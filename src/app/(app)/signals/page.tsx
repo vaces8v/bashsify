e7962b3eca7522d5a87306ee7f442c3ee4dd81e8
Vyacheslav Kholin <vaceslavupcotc@gmail.com>
2025-02-26 18:48:40 +0300

    add new elements

---
+++ src/app/(app)/signals/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { Button } from "@heroui/button"
+import { IterationCcw } from "lucide-react"
+import { useRouter } from "next/navigation"
+
+export default function Page() {
+    const router = useRouter()
+
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-full text-white">
+            <p className="text-3xl">Статья еще не готова,</p>
+            <p className="text-2xl">но она скоро будет доступна</p>
+            <Button onPress={() => router.push('/start')} className="mt-3 text-[18px]" startContent={<IterationCcw size={18}  className="mt-1"/>}>Перейти в начало</Button>
+        </div>
+    )
+}
